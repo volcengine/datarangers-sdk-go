5507cdb421388bfa9a84676bbac032374af25eb3
lixiuyang <lixiuyang@bytedance.com>
2020-08-25 15:36:45 +0800

    vendor依赖

--- event.pb.go
+++ event.pb.go
@@ -16,8 +16,11 @@ It has these top-level messages:
 */
 package pb_event
 
-import proto "github.com/golang/protobuf/proto"
-import math "math"
+//import proto "github.com/golang/protobuf/proto"
+import (
+	"github.com/golang/protobuf/proto"
+	math "math"
+)
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ = proto.Marshal
