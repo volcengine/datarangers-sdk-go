d3cf761ff1087ded3b76654b610d22f7552e11eb
lixiuyang <lixiuyang@bytedance.com>
2020-08-25 09:12:42 +0800

    配置文件方式配置

--- constant.go
+++ constant.go
@@ -1,12 +1,46 @@
 package mario_collector
 
+import (
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+)
+
 const (
-	//net setting
-	HttpAddr = "10.225.130.127"
 	AppURL = "/service/2/app_log"
 	WebURL = "/v2/event/json"
-
-	//log setting
-	ISLOG = true
-	LOGPATH = "kkkk.log"
 )
+
+var (
+	//net setting
+	//HttpAddr = "10.225.130.127"
+	HttpAddr string
+	//log setting
+	ISLOG bool
+	LOGPATH string
+)
+
+type conf struct {
+	Islog 		bool   		`yaml:"islog"` //yaml：yaml格式 enabled：属性的为enabled
+	Path    	string 		`yaml:"path"`
+	HttpAddr    string 		`yaml:"httpaddr"`
+	//AppURL    string `yaml:"path"`
+	//WebURL    string `yaml:"path"`
+}
+
+
+func init(){
+	yamlFile, err := ioutil.ReadFile("conf.yml")
+	if err != nil {
+		log.Printf("yamlFile.Get err   #%v ", err)
+	}
+	c := &conf{}
+	err = yaml.Unmarshal(yamlFile, c)
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+	}
+
+	ISLOG = c.Islog
+	HttpAddr = c.HttpAddr
+	LOGPATH = c.Path
+}
