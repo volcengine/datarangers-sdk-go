65c03db22f5296cd73ba28dbc78389a852e39178
lixiuyang <lixiuyang@bytedance.com>
2020-09-04 15:30:45 +0800

    v2版本

--- go.mod
+++ go.mod
@@ -3,12 +3,9 @@ module code.byted.org/data/datarangers-sdk-go
 go 1.14
 
 require (
-	github.com/donnie4w/go-logger v0.0.0-20170827050443-4740c51383f4
 	github.com/golang/protobuf v1.4.2
-	github.com/lestrrat-go/file-rotatelogs v2.3.0+incompatible
-	github.com/lestrrat-go/strftime v1.0.3 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	go.uber.org/zap v1.15.0
+	go.uber.org/zap v1.16.0
+	google.golang.org/protobuf v1.25.0 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 	gopkg.in/yaml.v2 v2.3.0
 )
--- main.go
+++ main.go
@@ -1,1 +1,33 @@
-package datarangers_sdk_go
+package main
+
+import (
+	sdk "code.byted.org/data/datarangers-sdk-go/event-collect"
+	"fmt"
+	"time"
+)
+
+func main() {
+	//err := sdk.AppCollect(10000013, "uuid", "newAppTest", map[string]interface{}{"param": 1}, map[string]interface{}{"cuns": 1})
+	//if err != nil {
+	//	fmt.Println(err.Error())
+	//}
+	aaa()
+	//bbb()
+}
+
+func aaa() {
+	for i := 0; i < 2; i++ {
+		err := sdk.AppCollectAsyn(10000013, "uuid", "newAppTest", map[string]interface{}{"param": 1}, map[string]interface{}{"cuns": 1})
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}
+	time.Sleep(2 * time.Second)
+}
+
+func bbb() {
+	err := sdk.AppCollect(10000013, "uuid", "newAppTest", map[string]interface{}{"param": 1}, map[string]interface{}{"cuns": 1})
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
