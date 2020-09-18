40190d3a03a66a3ab65abcac2017f3ee48f94be6
lixiuyang <lixiuyang@bytedance.com>
2020-09-18 11:28:02 +0800

    增加了debug日志
    Former-commit-id: f1c8e1fed057c9f402ca2b251d460fce562950d3

---
+++ mq.go
@@ -0,0 +1,39 @@
+package datarangers_sdk
+
+import (
+	"encoding/json"
+	"time"
+)
+
+type mq struct {
+	queue chan *dancemsg
+}
+
+func newMq() *mq {
+	mq1 := &mq{
+		queue: make(chan *dancemsg, confIns.Asynconf.Mqlen),
+	}
+	return mq1
+}
+
+func (p *mq) push(dmg *dancemsg) {
+
+	go func() {
+		select {
+		case p.queue <- dmg:
+			break
+		case <-time.After(30 * time.Second):
+			a, _ := json.Marshal(dmg)
+			warn("消息未进入队列，丢弃")
+			errlogger.Println(string(a))
+			break
+		}
+	}()
+}
+
+func (p *mq) pop() *dancemsg {
+	select {
+	case item := <-p.queue:
+		return item
+	}
+}
