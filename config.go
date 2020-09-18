50f8ddc2a9cbe1db8662646951c5facb3569d6c7
lixiuyang <lixiuyang@bytedance.com>
2020-09-18 11:28:43 +0800

    支持多种配置，增加默认配置
    Former-commit-id: 7e6fb2e378d6318836dfe1a246368b97c77af620

---
+++ config.go
@@ -0,0 +1,382 @@
+package datarangers_sdk
+
+import (
+	"encoding/json"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
+
+type apptype string
+type devicetype string
+
+const (
+	LevelTrace = iota
+	LevelDebug
+	LevelInfo
+	LevelNotice
+	LevelWarn
+	LevelError
+	LevelFatal
+
+	MP  apptype = "mp"
+	APP apptype = "app"
+	WEB apptype = "web"
+
+	IOS     devicetype = "IOS"
+	ANDROID devicetype = "ANDROID"
+)
+
+var (
+	defaultconf *Property
+	confIns     = &syncConf{}
+	headers     map[string]interface{}
+
+	logger    *log.Logger
+	errlogger *log.Logger
+
+	debuglog  = log.New(os.Stdout, "[DEBUG:]", log.Ldate|log.Ltime)
+	warnlog   = log.New(os.Stdout, "[WARN:]", log.Ldate|log.Ltime)
+	errstdlog = log.New(os.Stdout, "[ERROR:]", log.Ldate|log.Ltime)
+	loglevel  int
+
+	dialTimeout         = 30 * time.Second
+	totalTimeout        = 30 * time.Second
+	maxIdleConnsPerHost = 1024
+
+	isFirst               = true
+	isFirstConfByProperty = true
+	isFirstConfByFile     = true
+	isInit                = true
+
+	appcollector *mcsCollector
+	timezone     int
+
+	firstLock = sync.Mutex{}
+	mqlxy     *mq
+	instance  *execpool
+)
+
+type syncConf struct {
+	EventlogConfig eventlogConfig `yaml:"log"`
+	HttpConfig     httpConfig     `yaml:"http"`
+	Asynconf       asynconf       `yaml:"asyn"`
+}
+
+type eventlogConfig struct {
+	Islog      bool   `yaml:"islog"` //yaml：yaml格式 enabled：属性的为enabled
+	Iscollect  bool   `yaml:"iscollect"`
+	Path       string `yaml:"path"`
+	MaxSize    int    `yaml:"maxsize"` // megabytes
+	MaxBackups int    `yaml:"maxsbackup"`
+	MaxAge     int    `yaml:"maxage"` // days
+	ErrPath    string `yaml:"errlogpath"`
+	LogLevel   string `yaml:"loglevel"`
+}
+
+type httpConfig struct {
+	HttpAddr string `yaml:"addr"`
+}
+
+type asynconf struct {
+	Routine    int    `yaml:"routine"`
+	Errlogpath string `yaml:"errlogpath"`
+	Mqlen      int    `yaml:"mqlen"`
+	Mqwait     int    `yaml:"mqwait"`
+}
+
+//同步 异步 都会第一个初始化 此代码。
+//func initConfig() error {
+//	yamlFile, err := ioutil.ReadFile("sdkconf.yml")
+//	if err != nil {
+//		fatal(err)
+//		return err
+//	}
+//	confIns = &syncConf{}
+//	if err = yaml.Unmarshal(yamlFile, confIns); err != nil {
+//		fatal(err)
+//		return err
+//	}
+//	maps := map[string]map[string]interface{}{}
+//	if err = yaml.Unmarshal(yamlFile, &maps); err != nil {
+//		fatal(err)
+//		return err
+//	}
+//	headers = maps["headers"]
+//	newLog()
+//
+//	appcollector = newMcsCollector(confIns.HttpConfig.HttpAddr + "/sdk/log")
+//	timezone = getTimezone()
+//	loglevel, _ = strconv.Atoi(strings.ToUpper(confIns.EventlogConfig.LogLevel))
+//	switch strings.ToUpper(confIns.EventlogConfig.LogLevel) {
+//	default:
+//		loglevel = 3
+//	case "TRACE":
+//		loglevel = 0
+//	case "DEBUG":
+//		loglevel = 1
+//	case "INFO":
+//		loglevel = 2
+//	case "WARN":
+//		loglevel = 3
+//	case "ERROR":
+//		loglevel = 5
+//	case "FATAL":
+//		loglevel = 6
+//	}
+//	return nil
+//}
+
+func newLog() {
+	logger = &log.Logger{}
+	w := zapcore.AddSync(&lumberjack.Logger{
+		Filename:   confIns.EventlogConfig.Path,
+		MaxSize:    confIns.EventlogConfig.MaxSize, // megabytes
+		MaxBackups: confIns.EventlogConfig.MaxBackups,
+		MaxAge:     confIns.EventlogConfig.MaxAge, // days
+		LocalTime:  true,
+	})
+	logger.SetOutput(w)
+
+	if errlogger == nil {
+		errlogger = &log.Logger{}
+		q := zapcore.AddSync(&lumberjack.Logger{
+			Filename:   confIns.EventlogConfig.ErrPath,
+			MaxSize:    confIns.EventlogConfig.MaxSize, // megabytes
+			MaxBackups: confIns.EventlogConfig.MaxBackups,
+			MaxAge:     confIns.EventlogConfig.MaxAge, // days
+			LocalTime:  true,
+		})
+		errlogger.SetOutput(q)
+	}
+
+}
+
+func debug(s interface{}) {
+	if loglevel > LevelDebug {
+		return
+	}
+	debuglog.Println(s)
+}
+
+func warn(s interface{}) {
+	if loglevel > LevelWarn {
+		return
+	}
+	warnlog.Println(s)
+}
+
+func fatal(s interface{}) {
+	if loglevel > LevelError {
+		return
+	}
+	errstdlog.Println(s)
+}
+
+func initAsyn() {
+	mqlxy = newMq()
+	instance = newExecpool(confIns.Asynconf.Routine)
+	go instance.exec()
+}
+
+func InitByFile(path string) error {
+	if isFirstConfByFile {
+		firstLock.Lock()
+		defer firstLock.Unlock()
+		if isFirstConfByFile {
+			yamlFile, err := ioutil.ReadFile(path)
+			if err != nil {
+				fatal("初始化文件配置fail->  : " + err.Error() + ", 将使用默认配置")
+				return err
+			}
+			if err = yaml.Unmarshal(yamlFile, confIns); err != nil {
+				fatal("初始化文件配置fail->  : " + err.Error() + ", 将使用默认配置")
+				return err
+			}
+			maps := map[string]map[string]interface{}{}
+			if err = yaml.Unmarshal(yamlFile, &maps); err != nil {
+				fatal("headers配置错误： " + err.Error() + ", 将使用默认headers")
+				return err
+			}
+			headers = maps["headers"]
+			newLog()
+
+			appcollector = newMcsCollector(confIns.HttpConfig.HttpAddr + "/sdk/log")
+			timezone = getTimezone()
+			loglevel, _ = strconv.Atoi(strings.ToUpper(confIns.EventlogConfig.LogLevel))
+			switch strings.ToUpper(confIns.EventlogConfig.LogLevel) {
+			case "TRACE":
+				loglevel = 0
+				break
+			case "DEBUG":
+				loglevel = 1
+				break
+			case "INFO":
+				loglevel = 2
+				break
+			case "WARN":
+				loglevel = 3
+				break
+			case "ERROR":
+				loglevel = 5
+				break
+			case "FATAL":
+				loglevel = 6
+				break
+			default:
+				warn("loglevel 没有设置，默认为 0")
+			}
+			isFirstConfByFile = false
+			if a, err := json.Marshal(confIns); a != nil {
+				debug("自定义 File 配置 ：" + string(a))
+			} else {
+				fatal("自定义 File 配置 error : " + err.Error())
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func InitByProperty(p *Property) error {
+
+	if isInit || isFirstConfByProperty {
+		firstLock.Lock()
+		defer firstLock.Unlock()
+		if isInit || isFirstConfByProperty {
+			defaultconf.Log_islog = p.Log_islog
+			defaultconf.Log_iscollect = p.Log_iscollect
+			if p.Log_islog && p.Log_path != "" {
+				defaultconf.Log_path = p.Log_path
+			}
+			if p.Log_iscollect && p.Http_addr != "" {
+				defaultconf.Http_addr = p.Http_addr
+			}
+			if p.Log_errlogpath != "" {
+				defaultconf.Log_errlogpath = p.Log_errlogpath
+			}
+			if p.Log_maxsbackup != 0 {
+				defaultconf.Log_maxsbackup = p.Log_maxsbackup
+			}
+			if p.Log_maxsize != 0 {
+				defaultconf.Log_maxsize = p.Log_maxsize
+			}
+			if p.Log_maxage != 0 {
+				defaultconf.Log_maxage = p.Log_maxage
+			}
+			defaultconf.Log_loglevel = p.Log_loglevel
+			defaultconf.Headers = p.Headers
+			if p.Asyn_routine != 0 {
+				defaultconf.Asyn_routine = p.Asyn_routine
+			}
+			if p.Asyn_mqlen != 0 {
+				defaultconf.Asyn_mqlen = p.Asyn_mqlen
+			}
+			//根据这个初始化 confins。
+			confIns.EventlogConfig.Islog = defaultconf.Log_islog
+			confIns.EventlogConfig.Iscollect = defaultconf.Log_iscollect
+			confIns.EventlogConfig.LogLevel = defaultconf.Log_loglevel
+			confIns.EventlogConfig.ErrPath = defaultconf.Log_errlogpath
+			confIns.EventlogConfig.Path = defaultconf.Log_path
+			confIns.EventlogConfig.MaxAge = int(defaultconf.Log_maxage)
+			confIns.EventlogConfig.MaxBackups = int(defaultconf.Log_maxsbackup)
+			confIns.EventlogConfig.MaxSize = int(defaultconf.Log_maxsize)
+
+			confIns.HttpConfig.HttpAddr = defaultconf.Http_addr
+			confIns.Asynconf.Routine = int(defaultconf.Asyn_routine)
+			confIns.Asynconf.Mqlen = int(defaultconf.Asyn_mqlen)
+
+			if isInit {
+				if a, err := json.Marshal(confIns); a != nil {
+					debug("初始化默认配置 :" + string(a))
+				} else {
+					fatal("初始化默认配置error : " + err.Error())
+					return err
+				}
+			} else {
+				if a, err := json.Marshal(confIns); a != nil {
+					debug("自定义 Prooerty 配置 ：" + string(a))
+				} else {
+					fatal("自定义 Prooerty 配置error : " + err.Error())
+					return err
+				}
+				isFirstConfByProperty = false
+			}
+
+			//初始化其他参数
+			if p.Headers != nil {
+				headers = p.Headers
+			}
+			newLog()
+			appcollector = newMcsCollector(confIns.HttpConfig.HttpAddr + "/sdk/log")
+			timezone = getTimezone()
+			loglevel, _ = strconv.Atoi(strings.ToUpper(confIns.EventlogConfig.LogLevel))
+
+			switch strings.ToUpper(confIns.EventlogConfig.LogLevel) {
+			case "TRACE":
+				loglevel = 0
+				break
+			case "DEBUG":
+				loglevel = 1
+				break
+			case "INFO":
+				loglevel = 2
+				break
+			case "WARN":
+				loglevel = 3
+				break
+			case "ERROR":
+				loglevel = 5
+				break
+			case "FATAL":
+				loglevel = 6
+				break
+			default:
+				warn("loglevel 没有设置，默认为 0")
+			}
+		}
+	}
+	return nil
+}
+
+type Property struct {
+	Log_islog      bool
+	Log_iscollect  bool
+	Log_path       string
+	Log_errlogpath string
+	Log_maxsize    uint32
+	Log_maxsbackup uint32
+	Log_maxage     uint32
+	Log_loglevel   string
+	Http_addr      string //#上报的IP 或 域名, http://10.225.130.127
+	Headers        map[string]interface{}
+	Asyn_routine   uint32
+	Asyn_mqlen     uint32
+}
+
+//不初始化 按照默认配置走。
+func init() {
+	defaultconf = &Property{
+		Log_islog:      true,
+		Log_iscollect:  true,
+		Log_path:       "sdklogs/sensors",
+		Log_errlogpath: "sdklogs/errlog",
+		Log_loglevel:   "debug",
+		Log_maxage:     60,
+		Log_maxsize:    10,
+		Log_maxsbackup: 10,
+		Http_addr:      "http://10.225.130.127",
+		Headers:        map[string]interface{}{"host": "snssdk.vpc.com"},
+		Asyn_mqlen:     200000,
+		Asyn_routine:   1024,
+	}
+	InitByProperty(defaultconf)
+	isInit = false
+}
