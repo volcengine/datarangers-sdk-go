59daccf81740420d447236dd633cd7c9fe46bbe3
lixiuyang <lixiuyang@bytedance.com>
2020-09-18 11:29:22 +0800

    增加了debug日志，修改了接口名称
    Former-commit-id: 143c19b53f1154e3054e4710b0f97b9cfe8eaec8

---
+++ collect.go
@@ -0,0 +1,179 @@
+package datarangers_sdk
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	"io/ioutil"
+
+	//"io/ioutil"
+	"net"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type mcsCollector struct {
+	mscUrl        string
+	mscHttpClient *http.Client
+}
+
+func newMcsCollector(mcsUrl string) (collector *mcsCollector) {
+	collector = &mcsCollector{
+		mscUrl: mcsUrl,
+		mscHttpClient: &http.Client{
+			Transport: &http.Transport{
+				Dial: func(network, addr string) (net.Conn, error) {
+					return net.DialTimeout(network, addr, dialTimeout)
+				},
+				DisableKeepAlives:   false,
+				MaxIdleConnsPerHost: maxIdleConnsPerHost,
+				MaxIdleConns:        maxIdleConnsPerHost,
+			},
+			Timeout: totalTimeout,
+		},
+	}
+	return
+}
+
+func (p *mcsCollector) send(dmsg *dancemsg) error {
+	data, err := json.Marshal(dmsg)
+	if err != nil {
+		fatal(err.Error())
+		return err
+	}
+	req, err := http.NewRequest("POST", p.mscUrl, strings.NewReader(string(data)))
+	if err != nil {
+		fatal(err.Error())
+		return err
+	}
+	req.Header.Add("Content-Type", "application/json")
+	//req.Header.Add("Host", "snssdk.vpc.com")
+	//req.Host = "snssdk.vpc.com"
+	//defer req.Body.Close()
+	defer func() {
+		//ioutil.ReadAll(req.Body)
+		req.Body.Close()
+	}()
+	//补充 header
+	for k, v := range headers {
+		if m, okk := v.(string); okk {
+			req.Header.Set(k, m)
+			if strings.ToLower(k) == "host" {
+				req.Host = m
+			}
+		}
+		if m, okk := v.(int); okk {
+			req.Header.Set(k, strconv.Itoa(m))
+		}
+	}
+	h, _ := json.Marshal(req.Header)
+	debug("request header : " + string(h))
+
+	tmp, _ := json.Marshal(dmsg)
+	//fmt.Println(confIns.EventlogConfig.Islog)
+	if confIns.EventlogConfig.Islog {
+		//fmt.Println("保存成功！保存的json 为 -> : " + string(tmp))
+		debug("保存成功！保存的json 为 -> : " + string(tmp))
+		logger.Println(string(data))
+	}
+	var resp *http.Response
+	if confIns.EventlogConfig.Iscollect {
+		resp, err = p.mscHttpClient.Do(req)
+		if err != nil {
+			warn(err.Error() + "    消息未发送成功, 重试一次")
+			resp, err = p.mscHttpClient.Do(req)
+			if err != nil {
+				warn(err.Error() + "    重试时 未发送成功 ")
+			}
+		} else {
+			if resp.StatusCode != 200 {
+				fatal("信息发送失败，错误码为: " + strconv.Itoa(resp.StatusCode))
+				return fmt.Errorf("信息发送失败，错误码为: " + strconv.Itoa(resp.StatusCode))
+			} else {
+				debug("上报成功！上报的json 为 -> : " + string(tmp))
+			}
+		}
+		if resp != nil && resp.Body != nil {
+			ioutil.ReadAll(resp.Body)
+			resp.Body.Close()
+		}
+		//resp.Body.Close()
+	}
+	return err
+}
+
+func collectsync(apptype apptype, appid int64, uuid string, eventname string, eventParam map[string]interface{}, custom map[string]interface{}) error {
+	//if err := firstInit(); err != nil {
+	//	return err
+	//}
+	dmg := generate(appid, uuid, eventname, eventParam, custom, apptype)
+	if err := appcollector.send(dmg); err != nil {
+		data, a := json.Marshal(dmg)
+		if a != nil {
+			warn(err.Error() + "消息未发送成功")
+			return a
+		}
+		errlogger.Println(string(data))
+		return err
+	}
+	return nil
+}
+
+//func firstInit() error {
+//	if isFirst {
+//		firstLock.Lock()
+//		if isFirst {
+//			if err := initConfig(); err != nil {
+//				firstLock.Unlock()
+//				return err
+//			}
+//			isFirst = false
+//			firstLock.Unlock()
+//		}
+//	}
+//	return nil
+//}
+
+func generate(appid int64, uuid string, eventname string, eventParam map[string]interface{}, custom map[string]interface{}, apptype1 apptype) *dancemsg {
+	hd := &header{
+		Aid:            proto.Int64(appid),
+		Custom:         custom,
+		Device_id:      proto.Int64(1),
+		User_unique_id: proto.String(uuid),
+		Timezone:       proto.Int(timezone),
+	}
+
+	timeObj := time.Unix(time.Now().Unix(), 0)
+	itm := &items{
+		Datetime:    proto.String(timeObj.Format("2006-01-02 15:04:05")),
+		Event:       proto.String(eventname),
+		LocalTimeMs: proto.Int64(time.Now().UnixNano() / 1e6),
+		Params:      eventParam,
+	}
+	//time := &timeSync{
+	//	Local_time: proto.Int64(1),
+	//	Server_time: proto.Int64(1),
+	//}
+	dmg := &dancemsg{
+		App_id:         proto.Int64(appid),
+		User_unique_id: proto.String(uuid),
+		App_type:       proto.String(string(apptype1)),
+		Device_id:      proto.Int64(1),
+	}
+	dmg.Event_v3 = []*items{itm}
+	dmg.Header = hd
+	//dmg.TimeSync = time
+	return dmg
+}
+
+func getTimezone() int {
+	utc, _ := strconv.Atoi(time.Now().UTC().Format("2006-01-02 15:04:05")[11:13])
+	cur, _ := strconv.Atoi(time.Now().Format("2006-01-02 15:04:05")[11:13])
+	ans := cur - utc
+	if ans <= -12 {
+		ans = ans + 24
+	}
+	return ans
+}
